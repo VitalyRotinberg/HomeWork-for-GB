28c216f0107c024f3b71f37d0114e405ec71f223
 Виталя <vitalii@MacBook-Air-Vitala.local>
2022-04-08 19:21:49 +0500

    add task for homework

---
+++ Task/Program.cs
@@ -0,0 +1,31 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует 
+// массив строк, длина которых меньше либо равна 3 символа
+
+int index = 0;
+string [] arr = new string [] {"Hello", "World", "123", ".", "Holidays", "Day"};
+int count = 0;
+
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if(arr[i].Length<=3)
+        count++;
+}
+
+Console.WriteLine(count);
+
+string [] newArr = new string [count];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if(arr[i].Length<=3)
+    {
+        newArr[index] = arr[i];
+        index++;
+    }
+}
+
+for (int i = 0; i < newArr.Length; i++)
+{
+    Console.Write($"{newArr[i]}, ");
+}
